a637d2801ca2667b65c46eafa8a44925a7e91792
PeterP04 <peter140@uw.edu>
2026-03-30 10:07:41 -0700

    Update wdc.js

--- wdc.js
+++ wdc.js
@@ -1,55 +1,45 @@
 var myConnector = tableau.makeConnector();
 
-myConnector.getSchema = function (schemaCallback) {
-
-    var cols = [
-        { id: "id", dataType: tableau.dataTypeEnum.string },
-        { id: "mag", dataType: tableau.dataTypeEnum.float },
-        { id: "place", dataType: tableau.dataTypeEnum.string },
-        { id: "time", dataType: tableau.dataTypeEnum.datetime },
-        { id: "longitude", dataType: tableau.dataTypeEnum.float },
-        { id: "latitude", dataType: tableau.dataTypeEnum.float },
-        { id: "depth", dataType: tableau.dataTypeEnum.float }
-    ];
-
+myConnector.getSchema = function(schemaCallback) {
     schemaCallback([{
         id: "earthquakes",
-        columns: cols
+        columns: [
+            { id: "id", dataType: tableau.dataTypeEnum.string },
+            { id: "mag", dataType: tableau.dataTypeEnum.float },
+            { id: "place", dataType: tableau.dataTypeEnum.string },
+            { id: "time", dataType: tableau.dataTypeEnum.datetime },
+            { id: "longitude", dataType: tableau.dataTypeEnum.float },
+            { id: "latitude", dataType: tableau.dataTypeEnum.float },
+            { id: "depth", dataType: tableau.dataTypeEnum.float }
+        ]
     }]);
 };
 
-myConnector.getData = function (table, doneCallback) {
+myConnector.getData = function(table, doneCallback) {
 
-    var url =
-      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
-
-    fetch(url)
-        .then(response => response.json())
+    fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson")
+        .then(res => res.json())
         .then(data => {
 
-            var tableData = [];
-
-            data.features.forEach(feature => {
-
-                if (!feature || !feature.geometry || !feature.properties) return;
-
-                var coords = feature.geometry.coordinates || [];
-
-                tableData.push({
-                    id: feature.id,
-                    mag: feature.properties.mag,
-                    place: feature.properties.place,
-                    time: new Date(feature.properties.time),
-                    longitude: coords[0],
-                    latitude: coords[1],
-                    depth: coords[2]
-                });
-            });
+            let tableData = data.features.map(f => ({
+                id: f.id,
+                mag: f.properties.mag,
+                place: f.properties.place,
+                time: new Date(f.properties.time),
+                longitude: f.geometry.coordinates[0],
+                latitude: f.geometry.coordinates[1],
+                depth: f.geometry.coordinates[2]
+            }));
 
             table.appendRows(tableData);
             doneCallback();
-        })
-        .catch(err => console.error("WDC error:", err));
+        });
 };
 
 tableau.registerConnector(myConnector);
+
+// 🔥 REQUIRED
+document.getElementById("getData").addEventListener("click", function () {
+    tableau.connectionName = "USGS Earthquakes";
+    tableau.submitConnector();
+});
