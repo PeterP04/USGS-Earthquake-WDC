652553d9b0b0e064d70abff507418f127d246990
PeterP04 <peter140@uw.edu>
2026-03-30 09:55:29 -0700

    Refactor WDC schema and data fetching logic

--- wdc.js
+++ wdc.js
@@ -1,6 +1,5 @@
 var myConnector = tableau.makeConnector();
 
-// Define schema
 myConnector.getSchema = function (schemaCallback) {
 
     var cols = [
@@ -13,18 +12,16 @@ myConnector.getSchema = function (schemaCallback) {
         { id: "depth", dataType: tableau.dataTypeEnum.float }
     ];
 
-    var tableSchema = {
+    schemaCallback([{
         id: "earthquakes",
         columns: cols
-    };
-
-    schemaCallback([tableSchema]);
+    }]);
 };
 
-// Get data
 myConnector.getData = function (table, doneCallback) {
 
-    var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+    var url =
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
     fetch(url)
         .then(response => response.json())
@@ -33,26 +30,26 @@ myConnector.getData = function (table, doneCallback) {
             var tableData = [];
 
             data.features.forEach(feature => {
+
+                if (!feature || !feature.geometry || !feature.properties) return;
+
+                var coords = feature.geometry.coordinates || [];
+
                 tableData.push({
                     id: feature.id,
                     mag: feature.properties.mag,
                     place: feature.properties.place,
                     time: new Date(feature.properties.time),
-                    longitude: feature.geometry.coordinates[0],
-                    latitude: feature.geometry.coordinates[1],
-                    depth: feature.geometry.coordinates[2]
+                    longitude: coords[0],
+                    latitude: coords[1],
+                    depth: coords[2]
                 });
             });
 
             table.appendRows(tableData);
             doneCallback();
-        });
+        })
+        .catch(err => console.error("WDC error:", err));
 };
 
 tableau.registerConnector(myConnector);
-
-// Button click
-document.getElementById("getData").addEventListener("click", function () {
-    tableau.connectionName = "USGS Earthquake Feed";
-    tableau.submitConnector();
-});
